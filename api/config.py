a307be03bb336a2324a5e41780ec397d699d2594
Ryo-kun-1001 <r.maeda+github@pon3.dev>
2024-05-07 18:10:36 +0900

    docker-compose.ymlの環境変数化

--- api/config.py
+++ api/config.py
@@ -1,19 +1,20 @@
 from pydantic_settings import BaseSettings  # type: ignore
-from typing import Dict
+from pydantic import ConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "mysql+mysqlconnector://user:password@db:3306/mydatabase"
-    TEST_DATABASE_URL: str = "mysql+mysqlconnector://user:password@test_db:3307/test_db"
+    MYSQL_USER: str
+    MYSQL_PASSWORD: str
+    MYSQL_DATABASE: str
+    MYSQL_ROOT_PASSWORD: str
+    TEST_DATABASE: str
+    DATABASE_URL: str
+    TEST_DATABASE_URL: str
 
-    # テスト実行時にTrueにする
-    TESTING: bool = False
+    TESTING: bool = False  # テスト実行時にTrueにする
 
-    class Config:
-        config_dict: Dict[str, str] = {
-            "env_file": ".env",
-            # 必要に応じて他の設定もここに追加可能
-        }
+    class Config(ConfigDict):
+        env_file = ".env"
 
 
 # Settingsインスタンスの作成
